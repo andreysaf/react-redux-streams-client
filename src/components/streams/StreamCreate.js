e55142c822d92f177d82ce9b6cb907e96dc9635e
Andrey Safonov <a.so777@live.ca>
2019-08-18 17:16:35 -0700

    Added ability to add streams to a server, configured action creators for working with an API of streams

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends React.Component {
     renderError = ({error, touched}) => {
@@ -25,8 +27,8 @@ class StreamCreate extends React.Component {
     }
 
     onSubmit = (formValues) =>
-     {
-        console.log(formValues);
+    {
+        this.props.createStream(formValues);
     }
 
     render(){
@@ -51,7 +53,9 @@ const validate = formValues => {
     return errors;
 };
 
-export default reduxForm({
+const formWrapped =  reduxForm({
     form: 'streamCreate',
     validate: validate
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
