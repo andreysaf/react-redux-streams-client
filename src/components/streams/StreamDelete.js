65aff17f798299bee573dd1f2f31b4c93e5a99e6
Andrey Safonov <a.so777@live.ca>
2019-10-20 17:59:01 -0700

    Added a modal for deleting a stream, still WIP.

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import Modal from '../Modal';
 
 const SteamDelete = () => {
-    return <div>SteamDelete</div>;
+    const actions = (
+        <div>
+            <button className="ui red button">Delete</button>
+            <button className="ui button">Cancel</button>
+        </div>
+    );
+
+    return (
+        <div>
+            SteamDelete
+            <Modal 
+                title="Delete Stream" 
+                content="Are you sure you want to delete this stream?"
+                actions={actions}
+            />
+        </div>
+    );
 };
 
 export default SteamDelete;
