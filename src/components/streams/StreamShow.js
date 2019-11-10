88647b8b377828e4b7b3c039d0fff3565dbe805d
Andrey Safonov <a.so777@live.ca>
2019-11-09 19:25:06 -0800

    Setup streaming from OBS

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import flv from 'flv.js';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
 
 class StreamShow extends React.Component{
+    constructor(props) {
+        super(props);
+        this.videoRef = React.createRef();
+    }
+
     componentDidMount() {
-        this.props.fetchStream(this.props.match.params.id);
+        const videoId = this.props.match.params.id;
+        this.props.fetchStream(videoId);
+        this.buildPlayer();
+    }
+
+    componentDidUpdate() {
+        this.buildPlayer();
+    }
+
+    buildPlayer() {
+        if (this.player || !this.props.stream) {
+            return;
+        }
+
+        const videoId = this.props.match.params.id;
+        console.log(`http://localhost:8000/live/${videoId}.flv`);
+        this.player = flv.createPlayer({
+            type: 'flv',
+            isLive: true,
+            urL: `http://localhost:8000/live/${videoId}.flv`
+        });
+        this.player.attachMediaElement(this.videoRef.current);
+        this.player.load();
     }
 
     render() {
@@ -15,6 +43,7 @@ class StreamShow extends React.Component{
         const { title, description } = this.props.stream;
 
         return (<div>
+            <video ref={this.videoRef} style={{ width: '100%'}} controls />
             <h1>{title}</h1>
             <h5>{description}</h5>
         </div>);
